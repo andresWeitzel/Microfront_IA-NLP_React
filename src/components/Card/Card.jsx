544d9b8c26f4bd7bd33520685fadb114f1626498
Andrés Weitzel <andres96energy@hotmail.com>
2022-12-10 16:15:11 -0300

    Updated Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import img from '../../assets/images/cards/card03.webp';
 import modelRunner from 'gpt-j';
 
@@ -13,48 +14,51 @@ export default class Card extends React.Component {
 
 
     this.state = {
-      outputModel:[]
+      outputModel:'',
+      inputModel:''
     }
   }
 
-  runModel() {
-    let output = modelRunner.run(msg, apiKey, modelKey)
+  async runModel () {
+    let output = await modelRunner.run(msg, apiKey, modelKey)
     this.setState({
-      outputModel : output
+      outputModel : output.modelOutputs[0]
     })
     console.log(this.state.outputModel)
   }
 
 
   render() {
-    let { outputModel } = this.state.outputModel;
+    const { outputModel } = this.state;
 
     return (
       <>
-        <div class="container border-dark">
-          <div class="card bg-dark text-white">
-            <img src={img} class="card-img" alt="..." />
-            <div class="card-img-overlay">
-              <h5 class="card-title">Procesamiento de Lenguaje Natural | <span class="badge bg-dark">Modelo GPT-J-6B</span></h5>
-              <p class="card-text">Este es un Modelo de Procesamiento de Lenguaje Natural de código abierto. Detecta patrones según su entrada en tiempo real y genera una respuesta.</p>
-              <div class="container m-3">
-                <div class=" justify-content-center mb-3 mt-5 m-5">
-                  <label for="formGroupExampleInput" class="form-label"><h5>Input Modelo</h5></label>
-                  <div class="d-flex d-row ms-5 me-5">
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Entrada de Datos a Procesar" />
-                    <button type="submit" class="btn btn-primary" onClick={() => this.runModel()}>Enviar </button>
+        <div className="container border-dark">
+          <div className="card bg-dark text-white">
+            <img src={img} className="card-img" alt="..." />
+            <div className="card-img-overlay">
+              <h5 className="card-title">Procesamiento de Lenguaje Natural | <span className="badge bg-dark">Modelo GPT-J-6B</span></h5>
+              <p className="card-text">El Modelo GPT-J-6B es un modelo de Lenguaje Autorregresivo (representación de procesos aleatorios) de Procesamiento de Lenguaje Natural de código abierto. Detecta patrones según su entrada en tiempo real y genera una respuesta. Fue creado por el equipo <a href="https://www.eleuther.ai/" target="_blank" rel="noreferrer">EleutherAI</a> y es una de las alternativas más avanzadas en relación al modelo GPT-3 de <a href="https://openai.com/" target="_blank" rel="noreferrer">OpenAI.</a></p>
+              <div className="container m-3">
+                <div className=" justify-content-center mb-3 mt-5 m-5">
+                  <label for="formGroupExampleInput" className="form-label"><h5>Input Modelo</h5></label>
+                  <div className="d-flex d-row ms-5 me-5">
+                    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Entrada de Datos a Procesar" />
+                    <button type="submit" className="btn btn-primary" onClick={() => this.runModel()}>Enviar </button>
                   </div>
                 </div>
               </div>
-              <div class="container m-3">
-                <div class="card justify-content-center mb-3 mt-5 m-5">
-                  <div class="card-header bg-dark text-white">
+              <div className="container m-3">
+                <div className="card justify-content-center mb-3 mt-5 m-5">
+                  <div className="card-header bg-dark text-white">
                     Output Modelo
                   </div>
-                  <div class="card-body text-black">
-                    <blockquote class="blockquote mb-0">
-                      <p>{outputModel}</p>
-                      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                  <div className="card-body text-black">
+                    <blockquote className="blockquote mb-0">
+                      <p>{JSON.stringify(outputModel)}</p>
+                      
+                      <footer className="blockquote-footer mt-5">"La clave de la inteligencia artificial siempre ha sido la representación”.
+ <cite title="Source Title">Jeff Hawkins</cite></footer>
                     </blockquote>
                   </div>
                 </div>
